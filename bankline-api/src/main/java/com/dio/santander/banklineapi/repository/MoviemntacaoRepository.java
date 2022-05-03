bb03b5917a538f372191c216e256b5996a747e05
anderson olieira da silva <anderson.os88@hotmail.com>
2022-05-03 20:53:11 -0300

    santander-dev-week

---
+++ bankline-api/src/main/java/com/dio/santander/banklineapi/repository/MoviemntacaoRepository.java
@@ -0,0 +1,7 @@
+package com.dio.santander.banklineapi.repository;
+
+import com.dio.santander.banklineapi.model.Movimentacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MoviemntacaoRepository extends JpaRepository<Movimentacao, Integer> {
+}
